dc1498372d41ce9a44a4218ba069c78c78eda251
C Bares <colinedwinbares@gmail.com>
2022-04-14 16:37:57 -0500

    thought and reaction mutations

--- client/src/components/ThoughtForm/index.js
+++ client/src/components/ThoughtForm/index.js
@@ -1,28 +1,68 @@
 import { useState } from 'react';
-
-import React from 'react';
+import { useMutation } from '@apollo/client';
+import { ADD_THOUGHT } from '../../utils/mutations';
+import { QUERY_THOUGHTS, QUERY_ME } from '../../utils/queries'
 
 const ThoughtForm = () => {
   const [thoughtText, setText] = useState('');
   const [characterCount, setCharacterCount] = useState(0);
 
-  const handleChange = e => {
+  const [addThought, { error }] = useMutation(ADD_THOUGHT, {
+    update(cache, { data: { addThought } }) {
+      
+      try {
+        const { thoughts } = cache.readQuery({ query: QUERY_THOUGHTS });
+  
+        cache.writeQuery({
+          query: QUERY_THOUGHTS,
+          data: { thoughts: [addThought, ...thoughts] }
+        });
+      } catch(e) {
+        console.error(e);
+      }
+
+      const { me } = cache.readQuery({ query: QUERY_ME });
+      cache.writeQuery({
+        query: QUERY_ME,
+        data: { me: { ...me, thoughts: [...me.thoughts, addThought] } }
+      });
+    }
+  });
+
+  const handleChange = (e) => {
     if (e.target.value.length <= 280) {
       setText(e.target.value);
       setCharacterCount(e.target.value.length);
     }
   };
 
-  const handleFormSubmit = async e => {
-    e.preventDefault();
-    setText('');
-    setCharacterCount(0);
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      // add thought to database
+      await addThought({
+        variables: { thoughtText },
+      });
+
+      // clear form value
+      setText('');
+      setCharacterCount(0);
+    } catch (e) {
+      console.error(e);
+    }
   };
 
   return (
     <div>
-      <p className={`m-0 ${characterCount === 280 ? 'text-error' : ''}`}>Character Count: {characterCount}/280</p>
-      <form className="flex-row justify-center justify-space-between-md align-stretch" onSubmit={handleFormSubmit}>
+      <p className={`m-0 ${characterCount === 280 ? 'text-error' : ''}`}>
+        Character Count: {characterCount}/280
+        {error && <span className="ml-2">Something went wrong...</span>} 
+      </p>
+      <form
+        className="flex-row justify-center justify-space-between-md align-stretch"
+        onSubmit={handleFormSubmit}
+      >
         <textarea
           placeholder="Here's a new thought..."
           value={thoughtText}
